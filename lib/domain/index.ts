27eb1044120aba04fb96df9e656bff9c1b621289
Moriark <marcoa.r.aquino@gmail.com>
2026-09-15 05:11:02 -0300

    refactor(g5): expose canonical domain models

---
+++ lib/domain/index.ts
@@ -0,0 +1,31 @@
+export type {
+  Project,
+  ProjectCategory,
+  ProjectMaterial,
+  ProjectSpecs,
+} from "./project";
+
+export type {
+  Article,
+  ArticleCategory,
+  BlogTheme,
+} from "./article";
+
+export type {
+  ServiceItem,
+  DeliverableItem,
+  ProcessStep,
+  ProjectFormat,
+} from "./service";
+
+export type {
+  ContactMessage,
+  ContactStatus,
+  QuoteRequest,
+} from "./contact";
+
+export type {
+  PhoneContact,
+  SiteConfig,
+  SiteThemeId,
+} from "./site";
